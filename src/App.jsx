ac5fa814ca15050088c5b390ab5a432f41b07e3d
Pushpesh Almiya <almiyapushpesh55@gmail.com>
2024-10-17 09:46:57 +0530

    Quiz Application Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-2xl text-red-400 font-bold text-center'>Quiz App</h1>
+    </div>
+  )
+}
+
+export default App
